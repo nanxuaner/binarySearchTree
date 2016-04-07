cf9b116b5a7d0aff46585debb0b92ea4e5372c42
Anna <test@gmail.com>
2016-04-07 11:15:36 -0700

    add class Node

---
+++ binary_search_tree.py
@@ -0,0 +1,7 @@
+class Node:
+    """Represent a node of a binary tree"""
+    def __init__(self, key, value):
+        self.left = None
+        self.right = None
+        self.key = key
+        self.value = value
